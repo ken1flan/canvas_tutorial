e1521d799b72989730aeb671b44172ae6e11362a
ken1flan <ken1flan@gmail.com>
2020-12-06 12:18:52 +0900

    描画コンテキスト

--- 01_basic_usage/main.js
+++ 01_basic_usage/main.js
@@ -1,1 +1,9 @@
-console.log('hello, canvas!');
+function draw() {
+  var canvas = document.getElementById('tutorial');
+
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+  } else {
+    // TODO: canvas非対応の場合の代替コンテンツの処理
+  }
+}
