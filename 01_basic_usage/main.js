49c4d55cacf4a4ee729500d5c21bc1de86e02e73
ken1flan <ken1flan@gmail.com>
2020-12-06 12:26:31 +0900

    単純な描画

--- 01_basic_usage/main.js
+++ 01_basic_usage/main.js
@@ -3,6 +3,12 @@ function draw() {
 
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect (10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect (30, 30, 50, 50);
   } else {
     // TODO: canvas非対応の場合の代替コンテンツの処理
   }
